6ad7af7e19a9f8bb9bffe9088343df8362fee7fa
ambiga27 <cool.ambiga@gmail.com>
2025-01-09 02:41:15 +0530

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,23 +1,20 @@
 <?php 
-echo "hai";
-/*if(isset($_POST['submit'])){
-    $to = "email@example.com"; // this is your Email address
-    $name = $_POST['firstname'];
+if(isset($_POST['submit'])){
+    $to = "Letstalk@skssynergies.com"; // this is your Email address
+    $name = $_POST['name'];
     $phone = $_POST['phone'];
     $housename = $_POST['housename']; // this is the sender's Email address
     $city = $_POST['city']; // this is the sender's Email address
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $name . " " . $phone . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $housename . "\n\n" . $_POST['message'];
+    $subject = "Download free handbook";
+    
+    $message ="Name : ".$name . "\n phone :  " . $phone . " \n House Name : " .$housename."\n City : ".$city ;
 
-    echo $message;
 
     $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
     mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    
+    header('Location:index.html');
+//    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
-    }*/
+    }
 ?>
