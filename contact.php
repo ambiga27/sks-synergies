e1263481c07c315affe5d224d81643cc64111b89
ambiga27 <cool.ambiga@gmail.com>
2025-01-09 02:26:03 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,22 @@
+<?php 
+/*if(isset($_POST['submit'])){
+    $to = "email@example.com"; // this is your Email address
+    $name = $_POST['firstname'];
+    $phone = $_POST['phone'];
+    $housename = $_POST['housename']; // this is the sender's Email address
+    $city = $_POST['city']; // this is the sender's Email address
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $name . " " . $phone . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $housename . "\n\n" . $_POST['message'];
+
+    echo $message;
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }*/
+?>
